b0cfccbc64b070fc73c4e85d12baab70914dc2ee
Asmi Santhosh <asmisanthosh03@gmail.com>
2024-12-18 14:20:33 +0530

    Added visitor log management app with admin and security login features

---
+++ lib/pages/admin.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:visitor_app/pages/viewallvisitor.dart';
+
+class AdminPage extends StatelessWidget {
+  const AdminPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Admin Dashboard"),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.greenAccent
+          ),
+            onPressed: (){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context)=>Viewallvisitor()),
+              );
+            },
+          child: Text("View all Visitor Details",
+          ),
+        ),
+      ),
+    );
+  }
+}
